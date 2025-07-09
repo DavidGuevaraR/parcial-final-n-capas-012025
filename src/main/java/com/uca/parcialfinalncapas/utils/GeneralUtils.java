3551d77458982ddd41816ccbf08fb98127b2aa4d
David <00093217@uca.edu.sv>
2025-07-08 20:06:07 -0600

    refactor: Remove "/api/users" from EXCLUDED_PATHS in GeneralUtils

--- src/main/java/com/uca/parcialfinalncapas/utils/GeneralUtils.java
+++ src/main/java/com/uca/parcialfinalncapas/utils/GeneralUtils.java
@@ -2,6 +2,6 @@ package com.uca.parcialfinalncapas.utils;
 
 public class GeneralUtils {
     public static final String[] EXCLUDED_PATHS = {
-            "/api/auth/login", "/api/users"
+            "/api/auth/login"
     };
 }
